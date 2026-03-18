89118d6784dd18401a7f3d0b28aed1a5dbd2500b
Fuan200 <juandiazfdez1992@gmail.com>
2026-03-18 12:52:09 -0600

    create account endpoint

---
+++ schema/account.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import Generic, Optional, TypeVar
+from pydantic import BaseModel, ConfigDict, EmailStr
+from decimal import Decimal
+from uuid import UUID
+
+T = TypeVar("T")
+
+
+class AccountPublic(BaseModel):
+    id: UUID
+
+    created_at: datetime
+    updated_at: datetime
+
+    name: str
+    balance: Decimal
+    balance_include: bool
+    saving: bool
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class CreateAccount(BaseModel):
+    name: str
+    balance: Decimal
+    balance_include: Optional[bool] = None
+    saving: Optional[bool] = None
+    icon_id: Optional[UUID] = None
+
+
+class UpdateAccount(BaseModel):
+    name: Optional[str] = None
+    balance: Optional[Decimal] = None
+    balance_include: Optional[bool] = None
+    saving: Optional[bool] = None
+    icon_id: Optional[UUID] = None
+
+
+class SuccessResponse(BaseModel, Generic[T]):
+    success: bool = True
+    data: T
