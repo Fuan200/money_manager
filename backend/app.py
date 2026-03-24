6cb63835522cac2b200058d05054fbd16636104a
Fuan200 <juandiazfdez1992@gmail.com>
2026-03-24 16:21:42 -0600

    add cors

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from core.middleware import jwt_auth_middleware
 
 from routes.users import users
@@ -7,6 +8,15 @@ from routes.categories import categories
 from routes.transfers import transfers
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4321"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.middleware("http")
